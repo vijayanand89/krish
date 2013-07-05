15a7a6abecb03dfde8999a4e0b6145a181b546f7
vijayanand <support@weblogicxindia.com>
2013-07-05 17:10:28 +0530

    -Redesigned the front page -Added new modules

--- krish/index.php
+++ krish/index.php
@@ -8,13 +8,77 @@
  */
 
 defined('_JEXEC') or die;
-
 $app = JFactory::getApplication();
 $doc = JFactory::getDocument();
 $this->language = $doc->language;
 $this->direction = $doc->direction;
 $doc->addStyleSheet('templates/'.$this->template.'/css/bootstrap.css');
 $doc->addStyleSheet('templates/'.$this->template.'/css/template.css');
+
+if($this->countModules('content-right')) {
+	$content_span = 'span9';
+} else {
+	$content_span = 'span12';
+}
+
+if($this->countModules('bottom1 and bottom2 and bottom3 and bottom4')) {
+    $bottom_span = 3;
+}elseif( $this->countModules('bottom1 and bottom2 and bottom3') 
+		|| $this->countModules('bottom2 and bottom3 bottom4')
+		|| $this->countModules('bottom3 and bottom4 and bottom1')
+		) {
+	$bottom_span = 4;
+}elseif( $this->countModules('bottom1 and bottom2')
+		|| $this->countModules('bottom1 and bottom3')
+		|| $this->countModules('bottom1 and bottom4')
+		|| $this->countModules('bottom2 and bottom1')
+		|| $this->countModules('bottom2 and  bottom3')
+		|| $this->countModules('bottom2 and bottom4')
+		|| $this->countModules('bottom3 and bottom1')
+		|| $this->countModules('bottom3 and bottom2')
+		|| $this->countModules('bottom3 and  bottom4')
+		|| $this->countModules('bottom4 and  bottom1')
+		|| $this->countModules('bottom4 and bottom2')
+		|| $this->countModules('bottom4 and bottom3')
+		){
+	$bottom_span = 6;
+}elseif( $this->countModules('bottom1')
+		||$this->countModules('bottom2')
+		||$this->countModules('bottom3')
+		||$this->countModules('bottom4')
+		){
+	$bottom_span= 12;
+}
+
+if($this->countModules('footer1 and footer2 and footer3 and footer4')) {
+	$footer_span = 3;
+}elseif( $this->countModules('footer1 and footer2 and footer3')
+		|| $this->countModules('footer2 and footer3 footer4')
+		|| $this->countModules('footer3 and footer4 and footer1')
+) {
+	$footer_span = 4;
+}elseif( $this->countModules('footer1 and footer2')
+		|| $this->countModules('footer1 and footer3')
+		|| $this->countModules('footer1 and footer4')
+		|| $this->countModules('footer2 and footer1')
+		|| $this->countModules('footer2 and  footer3')
+		|| $this->countModules('footer2 and footer4')
+		|| $this->countModules('footer3 and footer1')
+		|| $this->countModules('footer3 and footer2')
+		|| $this->countModules('footer3 and  footer4')
+		|| $this->countModules('footer4 and  footer1')
+		|| $this->countModules('footer4 and footer2')
+		|| $this->countModules('footer4 and footer3')
+){
+	$footer_span = 6;
+}elseif( $this->countModules('footer1')
+		||$this->countModules('footer2')
+		||$this->countModules('footer3')
+		||$this->countModules('footer4')
+){
+	$footer_span=12;
+}
+
 ?>
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $this->language; ?>" lang="<?php echo $this->language; ?>" dir="<?php echo $this->direction; ?>">
@@ -32,7 +96,7 @@ $doc->addStyleSheet('templates/'.$this->template.'/css/template.css');
     <!-- header section starts -->
 	  <section class="header"><!-- header starts here --> 
 	    <div class="container"><!-- header container starts here -->
-	      <div class="row"><!--header logo row starts here -->
+	      <div class="row-fluid"><!--header logo row starts here -->
 	          <?php if($this->countModules('header-logo')): ?>
 	        <div class="span12"><!-- header logo span starts here -->
 	          <jdoc:include type="modules" name="header-logo" style="none" />
@@ -42,72 +106,136 @@ $doc->addStyleSheet('templates/'.$this->template.'/css/template.css');
 		    <div class="span12"><!-- navigation span starts here -->
 		    <center>
 			  <?php if($this->countModules('navigation')): ?>
-		     <div class="navbar">
-		      
-			   <jdoc:include type="modules" name="navigation" style="xhtml" />
-		       <ul class="nav ">
-		       </ul>
+			 <div class="navigation navbar">
+			   <jdoc:include type="modules" name="navigation" style="none" />
 		      </div>
 		      <?php endif; ?> 
 		    </div><!-- navigation span ends here -->
 	            
 	  </section>
 	   <!-- header section ends -->
-	   <!-- main content starts -->
-	   <section class="content-main"><!--main content section starts here-->
-		<div class="container"><!--content container starts here-->
-			
-			<jdoc:include type="message" />
-			<jdoc:include type="component" />
-			
-    	</div><!--content container ends here-->
-	   </section><!--main content section ends here-->
-	<!--main content ends -->
+	 
 	   <!-- slideshow starts -->
 	     <section class="slideshow">
-	      <div class="row">
 	      <?php if($this->countModules('slideshow')): ?>
-	      <jdoc:include type="modules" name="slideshow" style="xhtml" />
+	      <div class="row-fluid">
+	     <jdoc:include type="modules" name="slideshow" style="xhtml" />
 	      </div>
 	       <?php endif; ?>
 	      </section>
 	   <!-- slideshow ends -->
-	   <!-- content-top section starts -->
-		 <section class="content-top"><!--contetnt-top section starts here  -->
-		   <div class=container><!-- contetnt-top container starts here -->
-		      <?php if($this->countModules('content-top')): ?>
-		      <jdoc:include type="modules" name="content-top" style="xhtml" />
-		   </div><!-- contetnt-top container ends here -->
-		    <?php endif; ?> 
-		  </section><!-- contetnt-top sectionends here -->
-		  <!-- content-top section ends -->
-	  <!-- content-middle starts -->
-	  <section class="content-middle"><!-- content-middle section starts here -->
-	    <div class="container"><!-- content-middle container starts -->
-	  
-	  <?php if($this->countModules('content-middle')): ?>
-	      <jdoc:include type="modules" name="content-middle" style="xhtml" />
-	  
-	      </div><!-- content-middle container ends here -->
-	    <?php endif; ?>
-	  </section><!-- content-middle section ends -->
-	  <!-- content-middle ends -->
-	  <!-- content-bottom starts -->
-	  <section class="content-bottom">
-	    <?php if($this->countModules('content-bottom')): ?>
-	      <jdoc:include type="modules" name="content-bottom" style="xhtml" />
-	 </section>
-	  <?php endif; ?>
-	  <!-- content-bottom ends -->
-	  <!-- footer starts -->
-	  <footer>
-	  
-	 <?php if($this->countModules('footer')): ?>
-	      <jdoc:include type="modules" name="footer" style="xhtml" />
-	    
-	  
-	  </footer>
-	   <?php endif; ?>
-	  <!-- footer ends -->
+	   
+	<section class="content">
+		<div class="container">
+			<div class="row-fluid">
+				<div class="<?php echo $content_span; ?>">
+			   <!-- content-top section starts -->
+			      <?php if($this->countModules('content-top')): ?>
+			      	 <div class="content-top"><!--contetnt-top section starts here  -->
+					      <jdoc:include type="modules" name="content-top" style="xhtml" />
+					 </div><!-- contetnt-top sectionends here -->    
+				  <?php endif; ?> 
+				  <!-- content-top section ends -->
+			
+				  <!-- content-middle starts -->
+				  <?php if($this->countModules('content-middle')): ?>
+				 	  <div class="content-middle"><!-- content-middle section starts here -->
+				 	     <jdoc:include type="modules" name="content-middle" style="xhtml" />
+				     </div><!-- content-middle container ends here -->
+				    <?php endif; ?>
+					<div class="content-main">		    
+			   			<jdoc:include type="message" />
+						<jdoc:include type="component" />
+					</div>
+				  <!-- content-bottom starts -->
+				   <?php if($this->countModules('content-bottom')): ?>
+				   		<div class="content-bottom">
+				    	  <jdoc:include type="modules" name="content-bottom" style="xhtml" />
+				 		</div>
+				  <?php endif; ?>
+				  <!-- content-bottom ends -->
+				  
+			</div>
+		
+	  <!-- content-right starts -->
+	    <?php if($this->countModules('content-right')): ?>
+	    	<div class="span3 pull-right">
+	    	  <jdoc:include type="modules" name="content-right" style="xhtml" />
+	        </div>
+	       <?php endif; ?>
+	  <!-- content-right ends -->
+	  </div> <!-- content row ends -->
+	 </div> <!-- content container ends -->
+	</section> <!--  end of content section -->
+		<!-- bottom starts -->
+			<section>
+			  <div class="container">
+			    <div calss="row">
+			    <h3>WORK TEAM</h3>
+			      <?php if($this->countModules('bottom1')): ?>
+			      	 <div class="span<?php echo $bottom_span; ?>">
+					      <jdoc:include type="modules" name="bottom1" style="xhtml" />
+					 </div>   
+				  <?php endif; ?> 
+				  <?php if($this->countModules('bottom2')): ?>
+			      	 <div class="span<?php echo $bottom_span; ?>">
+					      <jdoc:include type="modules" name="bottom2" style="xhtml" />
+					 </div>   
+				  <?php endif; ?>
+				  <?php if($this->countModules('bottom3')): ?>
+			      	 <div class="span<?php echo $bottom_span; ?>">
+					      <jdoc:include type="modules" name="bottom3" style="xhtml" />
+					 </div>   
+				  <?php endif; ?>
+				  <?php if($this->countModules('bottom4')): ?>
+			      	 <div class="span<?php echo $bottom_span; ?>">
+					      <jdoc:include type="modules" name="bottom4" style="xhtml" />
+					 </div>   
+				  <?php endif; ?>				  
+			    </div>
+			  </div>
+			</section>
+		<!-- bottom ends -->  
+	 <!-- footer starts -->
+	   <footer>
+			  <div class="container">
+			    <div calss="row">
+			     <?php if($this->countModules('footer1')): ?>
+			      	 <div class="span<?php echo $footer_span; ?>">
+					      <jdoc:include type="modules" name="footer1" style="xhtml" />
+					 </div>   
+				  <?php endif; ?> 
+				  <?php if($this->countModules('footer2')): ?>
+			      	 <div class="span<?php echo $footer_span; ?>">
+					      <jdoc:include type="modules" name="footer2" style="xhtml" />
+					 </div>   
+				  <?php endif; ?>
+				  <?php if($this->countModules('footer3')): ?>
+			      	 <div class="span<?php echo $footer_span; ?>">
+					      <jdoc:include type="modules" name="footer3" style="xhtml" />
+					 </div>   
+				  <?php endif; ?>
+				  <?php if($this->countModules('footer4')): ?>
+			      	 <div class="span<?php echo $footer_span; ?>">
+					      <jdoc:include type="modules" name="footer4" style="xhtml" />
+					 </div>   
+				  <?php endif; ?>				  
+			    </div>
+			  </div>
+			</footer>
+			<!-- footer-logo starts -->
+	 	      <div class="container">
+			    <div class="row-fluid">
+			      <?php if($this->countModules('footer-logo')): ?>
+			    	<div class="span12">
+			          <div class="footer-logo">
+					   <jdoc:include type="modules" name="footer-logo" style="xhtml" />
+					  </div> 
+				   </div>
+			    </div>
+		      </div>   
+				  <?php endif; ?> 
+				  <!-- footer-logo ends -->
+	 <!-- footer ends -->
 </body>
 </html>
